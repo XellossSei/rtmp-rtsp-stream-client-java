0e03810843e04b2663c9fa924ffcb24eafa24588
GinRyan <lp8800lz@gmail.com>
2020-04-14 17:26:29 +0000

    功能基本上完成

---
+++ app/src/main/java/com/pedro/rtpstreamer/rtmpserver/NetworkUtils.java
@@ -0,0 +1,36 @@
+package com.pedro.rtpstreamer.rtmpserver;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * 网络工具
+ * 获得局域网IP地址
+ *
+ * @author dlong
+ * created at 2019/3/8 2:46 PM
+ */
+public class NetworkUtils {
+
+    /**
+     * 获取内网IP地址
+     *
+     * @return
+     * @throws SocketException
+     */
+    public static String getLocalIPAddress() throws SocketException {
+        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+            NetworkInterface intf = en.nextElement();
+            for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                InetAddress inetAddress = enumIpAddr.nextElement();
+                if (!inetAddress.isLoopbackAddress() && (inetAddress instanceof Inet4Address)) {
+                    return inetAddress.getHostAddress();
+                }
+            }
+        }
+        return "null";
+    }
+}
